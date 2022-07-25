01a17368d862b20b934c2893895e20d183f378a9
gLitCH1904 <95301749+gLitCH1904@users.noreply.github.com>
2022-07-25 20:42:40 +0100

    Add files via upload

---
+++ insure.js
@@ -0,0 +1,24 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-bar");
+const overlay = document.querySelector(".overlay");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  overlay.classList.add("active");
+});
+
+overlay.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  overlay.classList.remove("active");
+});
+
+for (let i = 0; i < navLink.length; i++) {
+  navLink[i].addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+}
